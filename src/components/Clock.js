0d2ea8f1980b6e48c6cb00edf9cda0eefb0cb379
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-01-05 00:42:27 -0800

    ADD

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -49,39 +49,3 @@ import React  from 'react';
 
 
 
-
-
-
-// import React, { Component } from 'react';
-
-// class App extends Component {
-
-//   constructor() {
-//     super()
-//     this.state={time:new Date()}
-//   }
-
-//   currentTime()
-//   {
-//     this.setState({
-//       time: new Date()
-//     })
-//   }
-
-//   componentWillMount()
-//   {
-//     setInterval(()=>this.currentTime(),1000)
-//   }
-
-
-//   render() {
-//     return (
-//       <h1> It is : 
-//         {this.state.time.toLocaleTimeString()}
-//       </h1>
-//     )
-//   }
-// }
-
-// export default App;
-
